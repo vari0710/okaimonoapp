febcf7566f6ef0367849d191fa3a1fa1be4d190d
vari0710 <himazinv0710@gmail.com>
2025-03-03 12:21:40 +0900

    js追加

--- javascript/contents.js
+++ javascript/contents.js
@@ -1,4 +1,17 @@
+function deleteAction(){
+    console.log('a');
+}
+
+const deleteButton = document.querySelector('button[name="delete"]');
+deleteButton.addEventListener('click',deleteAction);
+
+
 document.querySelector('button[name="cancel"]').addEventListener('click',
     () => {
-    window.alert('hello');
-})
+        //deleteActionの処理を解除する
+        deleteButton.removeEventListener('click',deleteAction);
+    }
+);
+
+
+
