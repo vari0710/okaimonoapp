19bbc0973e2f1c0861b2447410be9f0659279e40
vari0710 <himazinv0710@gmail.com>
2025-03-04 12:04:35 +0900

    menuのボタン追加完了、jsはまだ未完了

---
+++ Appearance/javascript/HamburgerMenu.js
@@ -0,0 +1,4 @@
+import { detailsAction } from "./fanction_contents.js";
+
+const detailsButton = document.querySelector('button[name = "Details"]');
+detailsButton.addEventListener('click',detailsAction);
